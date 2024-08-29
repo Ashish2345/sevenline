fe8fc73451c9ca7d748a5efe5cbc48681e85ffda
Ashish2345 <ashishrayamajhi227@gmail.com>
2024-08-30 01:13:25 +0545

    changes

--- servenline/urls.py
+++ servenline/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 
-from .views import (HomePageView, ContactPageView)
+from .views import (HomePageView, ContactPageView, ServicePageView, GeneralPageView,
+                    PreviewTipsPageView, GraphTipsPageView, SignupPageView, LoginPageView,
+                    PwResetPageView
+                    )
 
 app_name= "servenline"
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home_page"),
     path('contact_me.php', ContactPageView.as_view(), name="contact_page"),
+    
+    path('general', GeneralPageView.as_view(), name="general"),
+    path('preview_tip.php', PreviewTipsPageView.as_view(), name="preview_tip"),
+    path('graph_tip.php', GraphTipsPageView.as_view(), name="graph_tip"),
+    path('services.php', ServicePageView.as_view(), name="services"),
+
+    path('sign_up.php', SignupPageView.as_view(), name="signup"),
+    path('login.php', LoginPageView.as_view(), name="login"),
+    path('pw_reset.php', PwResetPageView.as_view(), name="pw_reset"),
    
 
 ]
--- servenline/views.py
+++ servenline/views.py
@@ -11,4 +11,25 @@ class HomePageView(TemplateView):
 class ContactPageView(TemplateView):
     template_name = "sevenline_main/contact_me.html"
 
-    
+
+class GeneralPageView(TemplateView):
+    template_name = "sevenline_main/general/index.html"
+
+class PreviewTipsPageView(TemplateView):
+    template_name = "sevenline_main/preview_tip.html"
+
+class GraphTipsPageView(TemplateView):
+    template_name = "sevenline_main/graph_tip.html"
+
+class ServicePageView(TemplateView):
+    template_name = "sevenline_main/services.html"
+
+class SignupPageView(TemplateView):
+    template_name = "sevenline_main/sign_up.html"
+
+class LoginPageView(TemplateView):
+    template_name = "sevenline_main/login.html"
+
+
+class PwResetPageView(TemplateView):
+    template_name = "sevenline_main/pw_reset.html"
